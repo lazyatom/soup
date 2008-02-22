1cfca797d5846f065c338bb29e392e033776ae07
James Adam <james@lazyatom.com>
2008-02-22 12:50:04 +0000

    We can now render dynasnips

--- render.rb
+++ render.rb
@@ -9,24 +9,25 @@ DataMapper::Persistence.auto_migrate!
 
 s1 = Snip.new(:name => "snip1")
 s1.content = <<EOF
-Linking is good: [snip2]
-Here's a super snip: {snip2}
+Linking is good: {link_to bold}
+Here's a bold snip: {bold}
+Here's a random number between 5 and 15: {rand 5,15}
 
 And lets include some textile: {textile_example}
-The source for that was <pre>{textile_example.content}</pre>
+The source for that was {pre textile_example}
 
 And lets include some markdown!: {markdown_example}
-The source for that was <pre>{markdown_example.content}</pre>
+The source for that was {pre markdown_example}
 
 EOF
 s1.save
 
-s2 = Snip.new(:name => "snip2")
-s2.content =<<EOF
+bold = Snip.new(:name => "bold")
+bold.content =<<EOF
 Snip2 in da house!
 EOF
-s2.render_as = "Bold"
-s2.save
+bold.render_as = "Bold"
+bold.save
 
 textile = Snip.new(:name => "textile_example")
 textile.content =<<EOF
@@ -41,11 +42,49 @@ textile = Snip.new(:name => "markdown_example")
 textile.content =<<EOF
 # testing header
 
-what the *hell* are __you__ looking at? [Beyotch](http://example.com)
+what the *hell* are looking at, [beyotch](http://example.com)?
 EOF
 textile.render_as = "Markdown"
 textile.save
 
+link_to = Snip.new(:name => "link_to")
+link_to.content =<<EOF
+class Linker
+  def handle(snip_name)
+    %{<a href="\#{snip_name}">\#{snip_name}</a>}
+  end
+end
+Linker
+EOF
+link_to.render_as = "Ruby"
+link_to.save
+
+pre = Snip.new(:name => "pre")
+pre.content =<<EOF
+class ShowContentInPreTag
+  def handle(snip_name)
+    %{<pre>\#{Snip.find_by_name(snip_name).content}</pre>}
+  end
+end
+ShowContentInPreTag
+EOF
+pre.render_as = "Ruby"
+pre.save
+
+rand = Snip.new(:name => "rand")
+rand.content =<<EOF
+class RandomNumber
+  def handle(min, max)
+    # arguments come in as strings, so we need to convert them.
+    min = min.to_i
+    max = max.to_i
+    (rand(max-min) + min)
+  end
+end
+RandomNumber
+EOF
+rand.render_as = "Ruby"
+rand.save
 
 module Render
   def self.class_called(renderer_name)
@@ -53,59 +92,86 @@ module Render
   end
 
   class Base
-    def rendering(snip, &block)
+    # Yields the appropriate renderer for the given snip. The default
+    # renderer is this one (Render::Base)
+    def rendering(snip, args=[])
       if renderer = snip.render_as
-        yield Render.class_called(renderer).new, snip
+        yield Render.class_called(renderer).new, snip, args
       else
-        yield self, snip
+        yield self, snip, args
       end
     end
     
-    # handles processing the text of the content
-    def process_text(str)
+    # Handles processing the text of the content. Subclasses should
+    # override this method to do fancy text processing
+    def process_text(str, args)
       str
     end
     
-    def include_snips(content)
-      content.gsub(/\{(\w+)\.?(\w+)?\}/) do
-        render($1, $2)
+    SNIP_REGEXP = re = %r{ \{
+      (\w+) (?: \.(\w+) )?
+      (?: \s+ ([\w,]+) )?
+    \} }x
+    
+    # Default behaviour to include a snip's content
+    def include_snips(content, args)
+      content.gsub(SNIP_REGEXP) do
+        snip_name = $1
+        snip_attribute = $2
+        snip_args = $3 ? $3.split(',') : []
+        render(snip_name, snip_attribute, snip_args)
       end
     end
     
-    def link_snips(content)
-      content.gsub(/\[(\w+)\]/) do
-        %{<a href="#{$1}">#{$1}</a>}
-      end
+    # Simply calls a bunch of methods, passing the result of each to the next
+    def chain(snip, args, *methods)
+      methods.inject(snip.content) { |str, method| send(method, str, args) }
     end
     
-    def render(snip_name, part=nil)
+    # Abstracted out because it might be useful to override somewhere
+    def render_part(snip, part, *args)
+      snip.__send__(part.to_sym)
+    end
+    
+    # Default rendering behaviour. Subclasses shouldn't really need to touch this.
+    def render(snip_name, part=nil, args=[])
       snip = Snip.find_by_name(snip_name)
       if part
-        snip.__send__(part.to_sym)
+        render_part(snip, part, args)
       else
-        rendering(snip) do |renderer, snip|
-          output = renderer.include_snips(snip.content)
-          output = renderer.link_snips(output)
-          output = renderer.process_text(output)
+        rendering(snip, args) do |renderer, snip, args|
+          renderer.chain snip, args, :process_text, :include_snips
         end
       end
     end
   end
 
+  # Snips that render_as "Ruby" should define a class which has the instance
+  # method 'handle' on it.
+  # The last line of the content should be the name of that class, so that it
+  # is returned by "eval" and we can instantiate it.
+  # The result always has #to_s called on it.
+  class Ruby < Base
+    def process_text(str, args)
+      handler = eval(str)
+      handler.new.handle(*args).to_s
+    end
+  end
+
   class Bold < Base
-    def process_text(str)
+    def process_text(str, args)
       "<b>#{str}</b>" 
     end
   end
   
   class Textile < Base
-    def process_text(str)
+    def process_text(str, args)
       RedCloth.new(str).to_html
     end
   end
   
   class Markdown < Base
-    def process_text(str)
+    def process_text(str, args)
       RedCloth.new(str).to_html(:markdown)
     end
   end
