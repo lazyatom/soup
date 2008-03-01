376305b19bd6e622764f82abbe8409d87220a481
James Adam <james@lazyatom.com>
2008-03-01 17:07:48 +0000

    We can use the format parameter to determine how to render the snip.

--- vanilla/snips_on_sinatra.rb
+++ vanilla/snips_on_sinatra.rb
@@ -23,54 +23,46 @@ module Router
   end
   
   def url_to(snip_name, part=nil)
-    url = "/space/#{snip_name}"
+    url = "/#{snip_name}"
     url += "/#{part}" if part
     url
   end
 
   def url_to_raw(snip_name, part=nil)
-    url = "/raw/#{snip_name}"
+    url = "/#{snip_name}"
     url += "/#{part}" if part
-    url
+    url += ".raw"
   end
   
   def edit_link(snip_name, link_text)
-    %[<a href="/space/edit?snip_to_edit=#{snip_name}">#{link_text}</a>]
+    %[<a href="/edit?snip_to_edit=#{snip_name}">#{link_text}</a>]
   end
   
   def new_link(snip_name="New")
-    %[<a href="/space/new" class="new">#{snip_name}</a>]
+    %[<a href="/new" class="new">#{snip_name}</a>]
   end
   
   extend self
 end
 
 helpers do
-  # render in main template
-  def show(snip_name)
-    Render.render('system', :main_template, [], params, Render::Erb)
-  end
-
-  # Render the content of this snip, but without recursing into other snips
-  def text(snip_name, part=nil)
-    Render.render_without_including_snips(snip_name, part || :content, [], params)
-  end
-
-  # Return the raw content of the snip (or snip part)
-  def raw(snip_name, part=nil)
-    Render.render(snip_name, part || :content, [], params, Render::Raw)
+  def present(snip_name, part=nil)
+    case params[:format]
+    when 'html'
+      # render in main template
+      Render.render('system', :main_template, [], params, Render::Erb)
+    when 'raw'
+      # Render the content of this snip, but without recursing into other snips
+      Render.render_without_including_snips(snip_name, part || :content, [], params)
+    when 'text'
+      # Return the raw content of the snip (or snip part)
+      Render.render(snip_name, part || :content, [], params, Render::Raw)
+    else
+      "Unknown format '#{params[:format]}'"
+    end
   end  
 end
 
 get('/') { redirect Router.url_to('start') }
-
-get('/space/:snip') { show params[:snip] }
-get('/space/:snip/:part') { show params[:snip] }
-   
-get('/raw/:snip') { raw params[:snip] }
-get('/raw/:snip/:part') { raw params[:snip], params[:part] }
-
-get('/text/:snip') { text params[:snip]  }
-get('/text/:snip/:part') { text params[:snip], params[:part] }
-
-
+get('/:snip') { present params[:snip] }
+get('/:snip/:part') { present params[:snip], params[:part] }
