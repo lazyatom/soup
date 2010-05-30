4c15d61487d94ec85bedc8c5257c941a98af2223
James Adam <james@lazyatom.com>
2010-05-31 01:01:42 +0200

    Avoid a warning with Ruby 1.9

--- lib/soup/empty_class.rb
+++ lib/soup/empty_class.rb
@@ -1,6 +1,6 @@
 # Based on Builder's BlankSlate object
 class Soup
   class EmptyClass
-    instance_methods.each { |m| undef_method(m) unless m =~ /^(__|instance_eval|respond_to\?)/ }
+    instance_methods.each { |m| undef_method(m) unless m =~ /^(__|object_id|instance_eval|respond_to\?)/ }
   end
 end
